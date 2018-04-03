63903ddcb65a684afd9029834fad4b019e87a9d2
galy <piguanghua@wordemotion.com>
2018-04-03 14:30:19 +0800

    异步搜索 code 完毕

--- src/main/scala/com/suty/currency/ch5/ParaSearch.scala
+++ src/main/scala/com/suty/currency/ch5/ParaSearch.scala
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import scala.collection.mutable.ListBuffer
 
+
 /**
   * Created by piguanghua on 4/2/18.
   */
@@ -12,19 +13,21 @@ import scala.collection.mutable.ListBuffer
 class SearchTask(val searchValue: Int, val begin: Int, val end: Int) extends Callable[Int] {
 
   def search(searchValue: Int, beginPos: Int, endPost: Int): Int = {
-    for (i <- beginPos to endPost) {
+    for (i <- beginPos until endPost) {
       if (SearchTask.result.get() >= 0) {
         SearchTask.result.get()
       }
 
 
       if (SearchTask.array(i) == searchValue) {
-        if (SearchTask.result.compareAndSet(-1, i))
+        //如果设置失败，其他线程已经找到了位置
+        if (SearchTask.result.compareAndSet(-1, i)) {
           SearchTask.result.get()
+        }
+        return i
       }
-      i
     }
-    -1
+    return -1
   }
 
   override def call(): Int = {
@@ -34,23 +37,41 @@ class SearchTask(val searchValue: Int, val begin: Int, val end: Int) extends Cal
 }
 
 object SearchTask {
-  val result = new AtomicInteger(-1)
-  val Thread_Num = 2
-  val pool = Executors.newCachedThreadPool()
-  var array = new Array[Int](3)
+  var result = new AtomicInteger(-1)
+  val Thread_Num = 5
+  val pool = Executors.newFixedThreadPool(10)
+  var array: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 0, 11, 34, 56, 7, 89, 90)
 
-  def pSearch(searchValue:Int)={
+  def pSearch(searchValue: Int): Int = {
     val subArraySize = array.length / Thread_Num + 1
-    var listBuffer = List[Future[Int]]
+    var re: ListBuffer[Future[Int]] = new ListBuffer[Future[Int]]
+
+
     var i = 0
-    while( i < array.length){
+    while (i < array.length) {
       var end = i + subArraySize
-      if(end >= array.length)
+      if (end >= array.length)
         end = array.length
 
+      val futureResult = pool.submit(new SearchTask(searchValue, i, end))
+      re.+=(futureResult)
 
-      //Java Future无此功能 +=
+      i += subArraySize
     }
+    Thread.sleep(1000)
+
+    for (future <- re) {
+      while (!future.isDone){}
+      if (future.get() > 0) {
+        return future.get()
+      }
+    }
+
+    -1
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(SearchTask.pSearch(4))
   }
 }
 
